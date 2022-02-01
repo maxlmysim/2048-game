a4d8f6f69b44680a875fa6ad53079ac83e3c4c16
maxlmysim <maxlmysim@gmail.com>
2022-02-01 19:35:17 +0300

    feat: add shift left, add newNum

---
+++ script/index.js
@@ -0,0 +1,115 @@
+function addNewNum() {
+
+    while (true) {
+        let checkFull = valueList.some((arrayX) => arrayX.includes(''));
+
+        if (!checkFull) {
+            break;
+        }
+
+        let firstNum = randomNumber();
+        let secondNum = randomNumber();
+
+        if (valueList[firstNum][secondNum] === '') {
+            valueList[firstNum][secondNum] = 2;
+            break;
+        }
+    }
+
+    updatePlayground();
+
+}
+
+function updatePlayground() {
+
+    valueList.forEach((arrayX, x) => {
+        arrayX.forEach((item, y) => {
+            playground.rows[x].cells[y].children[0].textContent = item;
+        });
+    });
+
+}
+
+function randomNumber() {
+    return Math.floor(Math.random() * 4);
+}
+
+function shiftLeft(arrayX) {
+
+    for (let i = 0; i < arrayX.length; i++) {
+
+        for (let j = i + 1; j < arrayX.length; j++) {
+
+            if (arrayX[j] === '') continue;
+
+            if (arrayX[i] !== arrayX[j]) break;
+
+            if (arrayX[i] === arrayX[j]) {
+                arrayX[i] += arrayX[j];
+                arrayX[j] = '';
+                break;
+            }
+        }
+    }
+
+    for (let i = 0; i < arrayX.length; i++) {
+
+        if (arrayX[i] !== '' && arrayX[i-1] === '') {
+            shiftHorizontal(arrayX, i, 'left');
+        }
+
+    }
+    return arrayX
+
+
+
+}
+
+function shiftRight() {
+
+    let arrayX = [];
+
+    for (let i = 3; i >= 0; i--) {
+
+    }
+
+}
+
+
+function shiftHorizontal(arr, pos, where) {
+
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === '') {
+                arr[i] = arr[pos];
+                arr[pos] = '';
+                return;
+            }
+        }
+
+}
+
+
+let valueList = [[8, 4, 2, ''], [8, 8, 16, 32], [32, 16, 8, 8], [4, 2, 2, '']];
+let valueList1 = [['', '', '', ''], ['', '', '', ''], ['', '', '', ''], ['', '', '', '']];
+
+let playground = document.querySelector('.playground');
+
+window.addEventListener('keyup', function (event) {
+    if (event.keyCode === 37) {
+        valueList.forEach(item => shiftLeft(item));
+        addNewNum()
+        updatePlayground()
+    }
+
+    if (event.keyCode === 39) {
+        shiftRight(0);
+        shiftRight(1);
+        shiftRight(2);
+        shiftRight(3);
+    }
+});
+
+updatePlayground();
+
+
+
